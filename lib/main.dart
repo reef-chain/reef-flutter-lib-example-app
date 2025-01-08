d5ad7fe2aa26cac685c5c39115a6707deea16ec5
Anukul Pandey <anukul.030601@gmail.com>
2025-01-08 14:58:36 +0530

    feat: function calls

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -52,16 +37,6 @@ class MyHomePage extends StatefulWidget {
 
 
   MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -72,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() async {
-
     widget.reefJsApiService.jsCall("window.isJsConn()").then((v)=>debugPrint(v.toString()));
     widget.reefJsApiService.jsCall("window.getReefJsVer()").then((v)=>debugPrint(v.toString()));
     // widget.reefJsApiService.jsCall("window.test()").then((v)=>debugPrint(v.toString()));
@@ -82,57 +56,49 @@ class _MyHomePageState extends State<MyHomePage> {
     await widget.reefChain.reefState.init(ReefNetowrk.testnet,[
       ReefAccount('name_acc1', '5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP', true)
     ]);
+
+
+    // logs tokens
     widget.reefChain.reefState.tokenApi.tokens.listen((v){
       var len = v.data.length.toString();
       debugPrint("tokens len=$len");
     });
 
+     // logs pools
+    widget.reefChain.reefState.poolsApi.fetchPools().then((val){
+      debugPrint("pools===$val");
+    });
+
+    // logs network
+    widget.reefChain.reefState.networkApi.selectedNetwork().listen((val){
+      debugPrint("selectedNetwork===$val");
+    });
+
+    // logs currencies from stealthex
+    widget.reefChain.reefState.stealthexApi.listCurrencies().then((val){
+      debugPrint("listCurrencies===$val");
+    });
+
+    // logs pool reserves
+    widget.reefChain.reefState.swapApi.getPoolReserves("0x70A82e21ec223c8691a0e44BEDC4790976Ea530c","0x0000000000000000000000000000000001000000").then((val){
+      debugPrint("getPoolReserves===$val");
+    });
+
 
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // widget.reefJsApiService.widget,
@@ -150,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
